2ed36be272294f5dee22e53bb73edb71f0832e1f
anaaabeatriz22 <120186991+anaaabeatriz22@users.noreply.github.com>
2023-10-18 16:33:39 -0300

    Add files via upload

---
+++ AtividadeTE-AnaBeatriz/Questão1-AnaB.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double raio;
+    double area;
+    double pi=3.14159;
+    cout<<"Digite o raio da circuferência: \n";
+    cin >> raio ;
+ 
+    area=(raio*raio)*pi;
+    cout<<"A área da circuferência é:"<<area;
+
+    return 0;
+}
---
+++ AtividadeTE-AnaBeatriz/Questão10-AnaB.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double num; 
+    double cubo; 
+    double quadrado;
+    cout<<"Digite um número:";
+    cin>>num;
+    quadrado=(num*num);
+    cubo=(num*num*num);
+    cout<<"O quadrado do número é: \n"<<quadrado<<"\n";
+    cout<<"O cubo do número é:\n"<<cubo;
+    
+
+    return 0;
+}
